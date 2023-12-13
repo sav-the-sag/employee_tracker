87d40b08222fbf12c951d1dabb03f46b739d1ea0
savan <savannahwilliams243@gmail.com>
2023-12-13 15:34:06 -0800

    created role table

--- db/schema.sql
+++ db/schema.sql
@@ -7,3 +7,13 @@ CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id) 
+    ON DELETE CASCADE
+);
