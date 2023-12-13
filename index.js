5caf84abfed50b88c4c23a852e02fdae59068855
savan <savannahwilliams243@gmail.com>
2023-12-13 09:26:50 -0800

    started dbConnection async fx

--- index.js
+++ index.js
@@ -9,3 +9,19 @@ require("dotenv").config();
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
 const dbName = process.env.DB_NAME;
+
+async function dbConnection(select) {
+    try {
+        const db = await mysql.createConnection({
+            host: "localhost",
+            user: dbUser,
+            password: dbPassword,
+            database: dbName,
+        });
+
+    }
+    catch (err) {
+        console.log(err);
+
+    }
+}
