1f21a0d0acd5e50674c6bab22828ba7aaa64c3a8
savan <savannahwilliams243@gmail.com>
2023-12-12 23:17:04 -0800

    env encryption and variables

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 // importing package files
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
+
+// Encryption for env file
+require("dotenv").config();
+
+// Dotenv variables
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbName = process.env.DB_NAME;
