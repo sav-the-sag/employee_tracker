a128680016f3dbf889c305f084597e6bc7c8cda3
savan <savannahwilliams243@gmail.com>
2023-12-13 15:37:03 -0800

    added data to role table

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,3 +4,17 @@ VALUES  ("Engineering"),
         ("Interconnected"),
         ("Sales"),
         ("Legal");
+
+INSERT INTO role (title, salary, department_id)
+VALUES  ("Engineering Manager", 120000, 1),
+        ("Engineering Lead", 100000, 1),
+        ("Staff Engineer", 80000, 1),
+        ("Finance Manager", 85000, 2),
+        ("Accountant", 70000, 2),
+        ("Brand Advocate Manager", 95000, 3),
+        ("Brand Advocate Sr. Analyst", 82000, 3),
+        ("Brand Advocate Analyst", 70000, 3),
+        ("Sales Manager", 75000, 4),
+        ("Salesperson", 65000, 4),
+        ("Legal Manager", 110000, 5),
+        ("Lawyer", 95000, 5);
