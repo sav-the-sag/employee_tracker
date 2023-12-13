64b7fc90787e8b0464d43e1b5b0db4d52c2b4e89
savan <savannahwilliams243@gmail.com>
2023-12-13 15:34:56 -0800

    created employee table

--- db/schema.sql
+++ db/schema.sql
@@ -17,3 +17,16 @@ CREATE TABLE role (
     REFERENCES department(id) 
     ON DELETE CASCADE
 );
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id), 
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
+);
