31ae8157bcc117d6f411db8c7b7fe65121a6383d
savan <savannahwilliams243@gmail.com>
2023-12-14 09:00:41 -0800

    add switch case for user input

--- index.js
+++ index.js
@@ -21,6 +21,28 @@ async function dbConnection(select) {
         // empty variables for query returns and prompt responses
         let returnedRowsFromDb = [];
         let returnedOutputFromInq = [];
+        // switch for all user input cases
+    switch (select) {
+        // id, name
+        case "View All Departments":
+          returnedRowsFromDb = await db.query("SELECT * FROM department");
+          console.table(returnedRowsFromDb[0]); // needs to be part of array?
+          break;
+  
+        // role id, job title, department value, salary value
+        case "View All Roles":
+          returnedRowsFromDb = await db.query(`
+                  SELECT
+                      role.id,
+                      role.title,
+                      role.salary,
+                      department.name AS department
+                  FROM role
+                  JOIN department ON role.department_id = department.id
+                  `);
+          console.table(returnedRowsFromDb[0]); 
+          break;
+    }
     }
     catch (err) {
         console.log(err);
